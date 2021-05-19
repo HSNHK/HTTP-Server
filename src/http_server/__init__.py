a0f058a1b99cc2a157b7df9eb8f092e545ed2311
HSNHK <hsnmm1379@gmail.com>
2021-05-19 15:02:34 +0430

    Module Level Dunder Names

--- src/http_server/__init__.py
+++ src/http_server/__init__.py
@@ -2,3 +2,6 @@ from http_server.requests import Requests
 from http_server.response import Response
 from http_server.server import WebServer
 from http_server.logging import Log
+
+__version__ = "0.1"
+__author__ = "https://github.com/HSNHK"

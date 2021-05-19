9e523b962f7ff810a6e20eca7fda8ce767937e11
HSNHK <hsnmm1379@gmail.com>
2021-05-19 14:48:36 +0430

    add not found and server error method

--- src/http_server/response.py
+++ src/http_server/response.py
@@ -1,14 +1,15 @@
 from socket import socket
 import json
 
+
 class Response:
-    def __init__(self,client:socket):
-        self.client=client
+    def __init__(self,client: socket):
+        self.client = client
 
-        self.headers=dict()
-        self.cookies=dict()
+        self.headers = dict()
+        self.cookies = dict()
 
-        self.__statusCode={
+        self.__statusCode = {
             200: "OK",
             201: "Created",
             202: "Accepted",
@@ -33,37 +34,43 @@ class Response:
             505: "HTTP Version Not Supported"
         }
 
-    def __sendResponse(self,content:str,status:int,contentType:str):
+    def __sendResponse(self,content: str,status: int,contentType: str):
         response_header = f"HTTP/1.1 {status} {contentType}\r\n"
         response_header += f"Server: simple http web Server\r\n"
         response_header += f"Content-Length: {len(content)}\r\n"
         response_header += f"Connection: close\r\n"
         response_header += f"{self.__createHeader}"
         response_header += f"Content-Type: {contentType}\r\n\r\n"
-
+        #send header and content
         self.client.send(response_header.encode("utf-8"))
         self.client.send(content.encode("utf-8"))
 
     @property
-    def __createHeader(self)->str:
+    def __createHeader(self) -> str:
         if len(self.headers)<1 and len(self.cookies)<1:
             return None
 
-        header_items="\r\n".join(f"{item[0]}:{item[1]}" for item in self.headers.items())
+        header_items = "\r\n".join(f"{item[0]}:{item[1]}" for item in self.headers.items())
 
-        cookie_items =f"set-cookie:"+";".join(f"{item[0]}={item[1]}" for item in self.cookies.items())
+        cookie_items = f"set-cookie:"+";".join(f"{item[0]}={item[1]}" for item in self.cookies.items())
 
         return f"{header_items}\r\n{cookie_items}\r\n"
     
-    def send(self,content:str,statusCode:int,contentType:str):
+    def send(self,content: str,statusCode: int,contentType: str):
         self.__sendResponse(content, statusCode, contentType)
     
-    def sendJson(self,content:dict,statusCode:int):
+    def sendJson(self,content: dict,statusCode: int):
         self.__sendResponse(json.dumps(content), statusCode, "application/json")
     
-    def sendJsonString(self,content:str,statusCode:int):
+    def sendJsonString(self,content: str,statusCode: int):
         self.__sendResponse(content, statusCode, "application/json")
     
-    def redirect(self,path:str):
+    def redirect(self,path: str):
         self.headers["Location"] = path
         self.__sendResponse("", 308, None)
+    
+    def notFound(self,message="Not Found"):
+        self.__sendResponse(f"<h1>{message}</h1>", 404, "text/html")
+
+    def internalServerError(self,message="Internal Server Error"):
+        self.__sendResponse(f"<h1>{message}<h1>", 500, "text/html")
